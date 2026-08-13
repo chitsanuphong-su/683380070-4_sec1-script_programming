bc1583be843655d47757914c3cf849756e5a565e
Chitsanuphong Su <chitsanuphongsukm@gmail.com>
2026-08-13 15:07:08 +0700

    Add Lab week6 folder

---
+++ week6/main_program.py
@@ -0,0 +1,9 @@
+import my_utils
+import math
+import random
+
+print(my_utils.greet("Kimmy"))
+print(my_utils.isprime(7))
+print(my_utils.isprime(10))
+print(math.sqrt(144))
+print(random.randint(1, 101))
---
+++ week6/my_utils.py
@@ -0,0 +1,7 @@
+from sympy import isprime
+
+def greet(name):
+  return f"Hello, {name}!"
+
+def is_prime(number):
+  return isprime(number)
