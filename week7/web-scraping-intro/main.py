422169a754e6a3591ef460f5e06ba89efb97f83e
Chitsanuphong Su <chitsanuphongsukm@gmail.com>
2026-09-23 17:34:24 +0700

    Add screenshot run program

--- week7/web-scraping-intro/main.py
+++ week7/web-scraping-intro/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 from scraper import AutomateBoringStuffScraper
 
 def main():
